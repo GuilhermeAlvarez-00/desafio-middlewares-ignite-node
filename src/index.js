f3af6925fbc822bb590660764f6d5eb962a1df81
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-09 18:59:55 -0300

    resolvendo middleware checksTodoExists

--- src/index.js
+++ src/index.js
@@ -34,7 +34,29 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  if(!validate(id)) {
+    return response.status(400)
+  }
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(404)
+  }
+
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
