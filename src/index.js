6d5c693131e17b9b3ea08d52393bda910839e542
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-09 18:05:31 -0300

    resolvendo middleware checksExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
