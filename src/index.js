a5b2412f52a01e768b669112d1090ed36210f689
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-09 19:09:17 -0300

    finalizando desafio

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function checksTodoExists(request, response, next) {
   const todo = user.todos.find(todo => todo.id === id)
 
   if(!todo) {
-    return response.status(404)
+    return response.status(404).json({ error: 'Todo not found' })
   }
 
   request.user = user
@@ -60,7 +60,16 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
